3dacac6433681bdeda6be6c03b067660961edd16
netsanet <netsanetbikes@gmail.com>
2022-11-12 23:38:25 -0800

    commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,26 +12,26 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, k, min;
 	int temp;
 
-	 for (i = 0; i < size - 1; i++)
-	 {
-		 min = i;
-		 for (j = i + 1; j < size; j++)
-		 {
-			 if (array[j] < array[min])
-				 min = j;
-		 }
-		 if (min != i)
-		 {
-			 temp = array[i];
-			 array[i] = array[min];
-			 array[min] = temp;
-			 for (k  = 0; k < size; k++)
-			 {
-				 if (k != size - 1)
-					 printf("%d, ", array[k]);
-				 else	
-					 printf("%d\n", array[k]);
-			 }
-		 }
-	 }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			for (k  = 0; k < size; k++)
+			{
+				if (k != size - 1)
+					printf("%d, ", array[k]);
+				else
+					printf("%d\n", array[k]);
+			}
+		}
+	}
 }
